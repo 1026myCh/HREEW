22a177c9b6f5933d365a1f2e2cd6da5abed68a15
1026myCh <723733650@qq.com>
2025-10-22 14:02:09 +0800

    Delete alg directory

--- alg/README.md
+++
@@ -1,4 +0,0 @@
-## cres-lme-python
-
-python4.0监控单元算法
-
--- alg/distance.py
+++
@@ -1,8 +0,0 @@
-from math import sin
-from math import cos
-from math import pi
-from math import acos
-
-def distance(epiLat,epiLon,Lat,Lon):
-    epi_dist = 6371.004*acos((sin(Lat/180*pi)*sin(epiLat/180*pi)+cos(Lat/180*pi)*cos(epiLat/180*pi)*cos((epiLon-Lon)/180*pi)))
-    return epi_dist
